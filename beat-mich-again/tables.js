6507217615d61591c9b44305ffb898448742829a
Bill Christie <william.christie@gmail.com>
2021-09-29 13:25:51 -0400

    wip

--- beat-mich-again/tables.js
+++ beat-mich-again/tables.js
@@ -89,6 +89,81 @@ function toItemMarkup(source) {
   `;
 }
 
+function setOverlaySizes() {
+  ['patio_layout', 'dining_layout', 'lane_layout' ].forEach(layoutId => {
+    var image = document.querySelector(`#${layoutId} img`);
+    var canvas = document.querySelector(`#${layoutId} canvas`);
+    canvas.width = image.width;
+    canvas.height = image.height;
+  });
+}
+
+function showLayoutGrid(id) {
+
+  var container = document.createElement('div');
+  container.className = 'layout-grid';
+
+  [...Array(100).keys()].map(i => {
+    var cell = document.createElement('div');
+    cell.className = `cell cell-${i}`;
+    return cell;
+  }).forEach(cell => {
+    container.append(cell);
+  });
+
+  var map = document.querySelector(`#${id} .map`);
+  map.append(container);
+}
+
+function writeSold() {
+
+  var canvas = document.getElementById('patio_overlay');
+  var ctx = canvas.getContext('2d');
+  ctx.textBaseline = 'middle';
+
+  var positions = [
+    {
+      id: 'table_1',
+      x: 20,
+      y: 20,
+      vertical: 1,
+      color: '#ff0',
+    },
+    {
+      id: 'table_5',
+      x: 20,
+      y: 26,
+      vertical: 0,
+      color: '#000',
+    },
+  ];
+
+  positions.forEach(position => {
+
+    var x = 0;
+    var y = 0;
+
+    if (position.vertical === 1) {
+      y = (position.x / 100) * canvas.width;
+      x = (position.y / 100) * canvas.height * -1;
+    } else {
+      x = (position.x / 100) * canvas.width;
+      y = (position.y / 100) * canvas.height;
+    }
+
+    var fontSize = canvas.height / 33;
+
+    ctx.save();
+    ctx.font = `bold ${fontSize}px arial`;
+    ctx.rotate(position.vertical * (-Math.PI/2));
+    ctx.fillStyle = position.color;
+    ctx.fillText('SOLD', x, y);
+    ctx.restore();
+
+  });
+
+}
+
 window.onload = function () {
   var container = document.getElementById('purchase_items');
   var markup = tables.map(toItemMarkup).join('');
@@ -101,4 +176,12 @@ window.onload = function () {
     console.log('purchase clicked', e.target.dataset.id);
 
   });
+
+  setOverlaySizes();
+
+  showLayoutGrid('patio_layout');
+  showLayoutGrid('dining_layout');
+  showLayoutGrid('lane_layout');
+
+  writeSold();
 }
