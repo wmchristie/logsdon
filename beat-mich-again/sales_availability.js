69835052357d109814430c5c596797aadfbb8c69
Bill Christie <william.christie@gmail.com>
2021-09-29 20:12:57 -0400

    wip

---
+++ beat-mich-again/sales_availability.js
@@ -0,0 +1,50 @@
+(function () {
+
+  var statuses = {
+    table_1: { available: true, },
+    table_2: { available: true, },
+    table_3: { available: true, },
+    table_4: { available: true, },
+    table_5: { available: true, },
+    table_6: { available: true, },
+    table_7: { available: true, },
+    table_8: { available: true, },
+    table_9: { available: true, },
+    table_0: { available: true, },
+
+    table_11: { available: true, },
+    table_12: { available: true, },
+    table_13: { available: true, },
+    table_14: { available: true, },
+    table_15: { available: true, },
+    table_16: { available: true, },
+    table_17: { available: true, },
+    table_18: { available: true, },
+    table_19: { available: true, },
+    table_20: { available: true, },
+
+    table_21: { available: true, },
+    table_22: { available: true, },
+    table_23: { available: true, },
+    table_24: { available: true, },
+    table_25: { available: true, },
+    table_26: { available: true, },
+    table_27: { available: true, },
+    table_28: { available: true, },
+    table_29: { available: true, },
+    table_30: { available: true, },
+
+    lane_1: { available: true, },
+    lane_2: { available: true, },
+    lane_3: { available: true, },
+    lane_4: { available: true, },
+    lane_5: { available: true, },
+    lane_6: { available: true, },
+    lane_7: { available: true, },
+    lane_8: { available: true, },
+  };
+
+  var app = window.app || (window.app = {});
+  app.tableStatus = statuses;
+
+}());
--- beat-mich-again/tables.js
+++ beat-mich-again/tables.js
@@ -1,187 +1,191 @@
-var tables = [
-  {
-    id: '58ZYFFK3TYJQ2',
-    name: 'Table 1',
-    description: 'High top 6 seats',
-    price: '$180',
-    is_available: true,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  },
-  {
-    id: 'table_5',
-    name: 'Table 2',
-    description: 'High top 6 seats',
-    price: '$240',
-    is_available: false,
-  }
-];
+(function () {
+  var app = window.app || (window.app = {});
 
-function toItemMarkup(source) {
-  return `
-    <div class="item">
-      <span class="name">${source.name}</span>
-      <span class="price">${source.price}</span>
-      <span class="description">${source.description}</span>
-      <button class="btn-purchase" data-purpose="purchase" data-id="${source.id}">Purchase</button>
-    </div>
-  `;
-}
-
-function setOverlaySizes() {
-  ['patio_layout', 'dining_layout', 'lane_layout' ].forEach(layoutId => {
-    var image = document.querySelector(`#${layoutId} img`);
-    var canvas = document.querySelector(`#${layoutId} canvas`);
-    canvas.width = image.width;
-    canvas.height = image.height;
-  });
-}
-
-function showLayoutGrid(id) {
-
-  var container = document.createElement('div');
-  container.className = 'layout-grid';
-
-  [...Array(100).keys()].map(i => {
-    var cell = document.createElement('div');
-    cell.className = `cell cell-${i}`;
-    return cell;
-  }).forEach(cell => {
-    container.append(cell);
-  });
-
-  var map = document.querySelector(`#${id} .map`);
-  map.append(container);
-}
-
-function writeSold() {
-
-  var canvas = document.getElementById('patio_overlay');
-  var ctx = canvas.getContext('2d');
-  ctx.textBaseline = 'middle';
-
-  var positions = [
+  var tables = [
     {
-      id: 'table_1',
-      x: 20,
-      y: 20,
-      vertical: 1,
-      color: '#ff0',
+      id: '58ZYFFK3TYJQ2',
+      name: 'Table 1',
+      description: 'High top 6 seats',
+      price: '$180',
+      is_available: true,
     },
     {
       id: 'table_5',
-      x: 20,
-      y: 26,
-      vertical: 0,
-      color: '#000',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
     },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    },
+    {
+      id: 'table_5',
+      name: 'Table 2',
+      description: 'High top 6 seats',
+      price: '$240',
+      is_available: false,
+    }
   ];
 
-  positions.forEach(position => {
+  function toItemMarkup(source) {
+    return `
+      <div class="item">
+        <span class="name">${source.name}</span>
+        <span class="price">${source.price}</span>
+        <span class="description">${source.description}</span>
+        <button class="btn-purchase" data-purpose="purchase" data-id="${source.id}">Purchase</button>
+      </div>
+    `;
+  }
 
-    var x = 0;
-    var y = 0;
+  function setOverlaySizes() {
+    ['patio_layout', 'dining_layout', 'lane_layout' ].forEach(layoutId => {
+      var image = document.querySelector(`#${layoutId} img`);
+      var canvas = document.querySelector(`#${layoutId} canvas`);
+      canvas.width = image.width;
+      canvas.height = image.height;
+    });
+  }
 
-    if (position.vertical === 1) {
-      y = (position.x / 100) * canvas.width;
-      x = (position.y / 100) * canvas.height * -1;
-    } else {
-      x = (position.x / 100) * canvas.width;
-      y = (position.y / 100) * canvas.height;
-    }
+  function showLayoutGrid(id) {
 
-    var fontSize = canvas.height / 33;
+    var container = document.createElement('div');
+    container.className = 'layout-grid';
 
-    ctx.save();
-    ctx.font = `bold ${fontSize}px arial`;
-    ctx.rotate(position.vertical * (-Math.PI/2));
-    ctx.fillStyle = position.color;
-    ctx.fillText('SOLD', x, y);
-    ctx.restore();
+    [...Array(100).keys()].map(i => {
+      var cell = document.createElement('div');
+      cell.className = `cell cell-${i}`;
+      return cell;
+    }).forEach(cell => {
+      container.append(cell);
+    });
 
-  });
+    var map = document.querySelector(`#${id} .map`);
+    map.append(container);
+  }
 
-}
+  function writeSold() {
 
-window.onload = function () {
-  var container = document.getElementById('purchase_items');
-  var markup = tables.map(toItemMarkup).join('');
-  container.innerHTML = markup;
+    var canvas = document.getElementById('patio_overlay');
+    var ctx = canvas.getContext('2d');
+    ctx.textBaseline = 'middle';
 
-  container.addEventListener('click', function (e) {
-    if (!e.target) return;
-    if (e.target.dataset.purpose !== 'purchase') return;
+    var positions = [
+      {
+        id: 'table_1',
+        x: 20,
+        y: 20,
+        vertical: 1,
+        color: '#ff0',
+      },
+      {
+        id: 'table_5',
+        x: 20,
+        y: 26,
+        vertical: 0,
+        color: '#000',
+      },
+    ];
 
-    console.log('purchase clicked', e.target.dataset.id);
+    positions.forEach(position => {
 
-  });
+      var x = 0;
+      var y = 0;
 
-  setOverlaySizes();
+      if (position.vertical === 1) {
+        y = (position.x / 100) * canvas.width;
+        x = (position.y / 100) * canvas.height * -1;
+      } else {
+        x = (position.x / 100) * canvas.width;
+        y = (position.y / 100) * canvas.height;
+      }
 
-  showLayoutGrid('patio_layout');
-  showLayoutGrid('dining_layout');
-  showLayoutGrid('lane_layout');
+      var fontSize = canvas.height / 33;
 
-  writeSold();
-}
+      ctx.save();
+      ctx.font = `bold ${fontSize}px arial`;
+      ctx.rotate(position.vertical * (-Math.PI/2));
+      ctx.fillStyle = position.color;
+      ctx.fillText('SOLD', x, y);
+      ctx.restore();
+
+    });
+
+  }
+
+  window.onload = function () {
+    var container = document.getElementById('purchase_items');
+    var markup = tables.map(toItemMarkup).join('');
+    container.innerHTML = markup;
+
+    container.addEventListener('click', function (e) {
+      if (!e.target) return;
+      if (e.target.dataset.purpose !== 'purchase') return;
+
+      console.log('purchase clicked', e.target.dataset.id);
+
+    });
+
+    setOverlaySizes();
+
+    showLayoutGrid('patio_layout');
+    showLayoutGrid('dining_layout');
+    showLayoutGrid('lane_layout');
+
+    writeSold();
+  }
+}());
